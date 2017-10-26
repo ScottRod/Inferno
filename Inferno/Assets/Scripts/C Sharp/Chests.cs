496c4a6691a0bb203e38e6995187e7956bbe9339
ScottRod <32605541+ScottRod@users.noreply.github.com>
2017-10-26 19:59:57 +1100

    Chest
    Added a Chest image, and a started making the chest script

---
+++ Inferno/Assets/Scripts/C Sharp/Chests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chests : MonoBehaviour {
+
+	public string ItemName; // work it out
+
+	public GameObject ItemObj;// the item's prefab, to show the item in the chest
+
+	GameObject newItemObj;
+
+	// Use this for initialization
+	void Start () {
+
+		 newItemObj = Instantiate (ItemObj, transform.position + Vector3.down, Quaternion.identity); 
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		newItemObj.transform.Rotate (0, 30 * Time.deltaTime, 0);
+		
+	}
+
+	public void GetItem() {
+
+		PlayerPrefs.SetInt (ItemName, 1);
+
+	}
+
+}
